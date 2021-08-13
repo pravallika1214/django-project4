497046dcd30e5eaf6f6d0814e5b9d52448744c46
valli1214 <vallivadlamudi@gmail.com>
2021-08-13 02:13:49 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pravallika(request):
+    return render(request,'pravallika.html')
+def pravallika1(request):    
+    return render(request,'pravallika1.html')
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pravallika1(request):
+    return render(request,'pravallika1.html')
